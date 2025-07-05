c41dc1a95aac303e751c908ead7d0098822ae506
amina19058 <a.khusnutdinova@innopolis.university>
2025-07-06 00:24:54 +0300

    dummy collection

--- Tracker/TrackersViewController.swift
+++ Tracker/TrackersViewController.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 class TrackersViewController: UIViewController {
+    var categories: [TrackerCategory] = []
+    var completedTrackers: [TrackerRecord] = []
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        return collectionView
+    }()
+    
+    private let letters = [
+                "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к",
+                "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц",
+                "ч", "ш" , "щ", "ъ", "ы", "ь", "э", "ю", "я"
+    ]
     
     private let titleLabel: UILabel = {
         let titleLabel = UILabel()
@@ -21,10 +35,12 @@ class TrackersViewController: UIViewController {
     
     private let searchBar: UISearchBar = {
         let searchBar = UISearchBar()
+        
         searchBar.placeholder = .Labels.searchFieldPlaceholder
         searchBar.searchBarStyle = .minimal
         searchBar.layer.cornerRadius = 10
         searchBar.clipsToBounds = true
+        
         return searchBar
     }()
     
@@ -39,7 +55,8 @@ class TrackersViewController: UIViewController {
         setupNavigationBar()
         setupTitleLabel()
         setupSearchBar()
-        setupStub()
+//        setupStub()
+        setupCollectionView()
     }
     
     private func setupNavigationBar() {
@@ -111,11 +128,85 @@ class TrackersViewController: UIViewController {
         stubTextLabel.accessibilityIdentifier = .AccessibilityIdentifiers.stubLabel
     }
     
+    private func setupCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.register(TrackerCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.register(SupplementaryView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "header")
+        
+        collectionView.allowsMultipleSelection = false
+        
+        view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 24),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+    }
+    
     @objc private func addTrackerTapped() {
 
     }
     
     @objc private func datePickerTapped() {
 
+    }
+}
+
+extension TrackersViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return letters.count
+    }
+        
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? TrackerCell
+        
+        cell?.titleLabel.text = letters[indexPath.row]
+        return cell!
+    }
+}
+
+extension TrackersViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        var id: String
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            id = "header"
+        default:
+            id = ""
+        }
+        
+        let view = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: id, for: indexPath) as! SupplementaryView // 6
+        view.titleLabel.text = "Здесь находится Supplementary view"
+        return view
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as? TrackerCell
+        cell?.titleLabel.font = UIFont.boldSystemFont(ofSize: 17)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as? TrackerCell
+        cell?.titleLabel.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+    }
+}
+
+extension TrackersViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        CGSize(width: collectionView.frame.width, height: 55)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize { // 1
+        return CGSize(width: collectionView.bounds.width / 2, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
 }
